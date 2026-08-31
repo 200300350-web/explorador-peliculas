98f95f25085a1e514c907fa2827a4753855de126
Diego Rodriguez <200300350@ucaribe.edu.mx>
2026-08-31 13:53:48 -0500

    fix: guardar backticks de verdad y agregar log

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
 
 export const fetchPopularMovies = async () => {
+  console.log("Vercel BASE_URL:", BASE_URL); // <-- Esta línea es clave
   try {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
